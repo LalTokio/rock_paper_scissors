0f83c2a31d3c9716e34234df8351114dcfd63554
LalTokio <149398272+LalTokio@users.noreply.github.com>
2024-01-30 10:37:18 -0400

    updated steps 3 to 5

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 
-
-
 const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-const choice= options[Math.floor(Math.random() * options.length)]; 
-return choice
+let choice= options[Math.floor(Math.random() * options.length)]; 
+return choice;
 }
 
 
+function playRound(playerSelection, computerSelection) { 
+if (playerSelection==computerSelection){
+return "tie";
+}else if (playerSelection == "rock" && computerSelection == "scissors"){
+    return "you win rock beats scissors";
+}else if (playerSelection == "paper" && computerSelection == "rock"){
+    return "you win paper beats rock";
+}else if (playerSelection == "scissors" && computerSelection == "paper"){
+    return "win scissors beats paper";
+}else if (playerSelection == "rock" && computerSelection == "paper"){
+    return "you lose paper beats rock";
+}else if (playerSelection == "paper" && computerSelection == "scissors"){
+    return "you lose scissors beats paper";
+}else if (playerSelection == "scissors" && computerSelection == "rock"){
+    return "you lose rock beats scissors";
+}
+}
 
-function playRound(playerSelection, computerSelection) {
-   
-  }
+const playerSelection = prompt("Choose your option").toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+let playerScore= 0;
+let computerScore= 0;
+
+function playGame(){}
