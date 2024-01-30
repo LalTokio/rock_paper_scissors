a0c5460ed81bc27e6c4044df8427b0b10f333105
LalTokio <149398272+LalTokio@users.noreply.github.com>
2024-01-30 08:36:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,17 +8,7 @@ const choice= options[Math.floor(Math.random() * options.length)];
 return choice
 }
 
-function verifyWinner(){
-    if(playerSelection == computerSelection){
-        return "Tie";
-    }else if (playerSelection == "rock" && computerSelection == "scissors" ||
-    playerSelection == "paper" && computerSelection == "rock" ||
-    playerSelection == "scissors" && computerSelection == "paper"){
-        return "Player";
-    } else{
-        return "Computer";
-    }
-}
+
 
 function playRound(playerSelection, computerSelection) {
    
