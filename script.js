d14e4c189e14c50bed44c04fffdd3fa9f3c2d8a1
22miguel <22miuel.gallado@gmail.com>
2024-01-31 10:36:27 -0400

    feat: finish part 6

--- script.js
+++ script.js
@@ -10,34 +10,33 @@ function playGame(){
 let playerScore= 0;
 let computerScore= 0;  
 for (let i= 0; i < 5; i++) {
+    const playerSelection = prompt("Choose your option").toLowerCase();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
     function playRound(playerSelection, computerSelection) { 
-        if (playerSelection==computerSelection){
+        if (playerSelection === computerSelection){
         return "tie";
-        }else if (playerSelection == "rock" && computerSelection == "scissors"){
+        }else if (playerSelection === "rock" && computerSelection === "scissors"){
             playerScore++;
             return "you win rock beats scissors";
-        }else if (playerSelection == "paper" && computerSelection == "rock"){
+        }else if (playerSelection === "paper" && computerSelection === "rock"){
             playerScore++;
             return "you win paper beats rock";
-        }else if (playerSelection == "scissors" && computerSelection == "paper"){
+        }else if (playerSelection === "scissors" && computerSelection === "paper"){
             playerScore++;
             return "win scissors beats paper";
-        }else if (playerSelection == "rock" && computerSelection == "paper"){
+        }else if (playerSelection === "rock" && computerSelection === "paper"){
             computerScore++;
             return "you lose paper beats rock";
-        }else if (playerSelection == "paper" && computerSelection == "scissors"){
+        }else if (playerSelection === "paper" && computerSelection === "scissors"){
             computerScore++;
             return "you lose scissors beats paper";
-        }else if (playerSelection == "scissors" && computerSelection == "rock"){
+        }else if (playerSelection === "scissors" && computerSelection === "rock"){
             computerScore++;
             return "you lose rock beats scissors";
         }
     }
-    const playerSelection = prompt("Choose your option").toLowerCase();
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
 }
-console.log("The Game is Over")
 if (playerScore < computerScore){
     console.log("Computer Wins");
 }else if (playerScore > computerScore){
